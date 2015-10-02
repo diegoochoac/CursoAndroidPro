38a47a8af157a5b6d50b406080f430f54bb4d725
diegoochoac <diego.ochoa@correounivalle.edu.co>
2015-10-01 21:39:21 -0500

    commit n.2 (auditor de camponombre)

--- src/com/example/app/MainActivity.java
+++ src/com/example/app/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.app;
 
+import com.example.app.util.TextChangedListener;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+	private EditText txtNombre, txtTelefono, txtEmail, txtDireccion;
+	private Button btnAgregar;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializarComponentesUI();
     }
 
+
+	private void inicializarComponentesUI() {
+		txtNombre = (EditText) findViewById(R.id.cmpNombre);
+		txtTelefono = (EditText) findViewById(R.id.cmpTelefono);
+		txtEmail = (EditText) findViewById(R.id.cmpEmail);
+		txtDireccion = (EditText) findViewById(R.id.cmpDireccion);
+		
+		txtNombre.addTextChangedListener(new TextChangedListener(){
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+					btnAgregar = (Button) findViewById(R.id.btnAgregar);
+					btnAgregar.setEnabled(!s.toString().trim().isEmpty());
+			}
+		});
+	}
+
+	public void onClick(View view){
+		String mesg = String.format("%s ha sido agregado a la lista", txtNombre.getText());
+		Toast.makeText(this, mesg, Toast.LENGTH_SHORT).show();
+		btnAgregar.setEnabled(false);
+		limpiarCampos();
+	}
+
+
+	private void limpiarCampos() {
+		txtNombre.getText().clear();
+		txtTelefono.getText().clear();
+		txtDireccion.getText().clear();
+		txtEmail.getText().clear();
+		txtNombre.requestFocus();
+	}
+	
 }
---
+++ src/com/example/app/util/TextChangedListener.java
@@ -0,0 +1,27 @@
+package com.example.app.util;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public class TextChangedListener implements TextWatcher  {
+
+	@Override
+	public void afterTextChanged(Editable s) {
+		
+		
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		
+		
+	}
+
+}
